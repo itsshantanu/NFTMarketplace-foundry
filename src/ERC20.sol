98c9db5ff266bc9b6721c9c85c9c41f6947b446f
arjun <arjun232001@gmail.com>
2024-10-01 19:21:22 +0530

    ds

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-//test
 contract MoonToken is ERC20 {
     address public admin;
     constructor() ERC20("MoonToken", "MOON") {
